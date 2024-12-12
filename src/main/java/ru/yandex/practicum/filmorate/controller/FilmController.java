30eaa678950c6e74204602cb2aa5c0b1b338b5da
xawk <xawk0469@yandex.ru>
2024-12-12 13:47:45 +0300

    fix: checkstyle in FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 public class FilmController {
 
     private final Map<Long, Film> films = new LinkedHashMap<>();
-    private final LocalDate DAYOFF = LocalDate.of(1895, 12, 28);
+    private final LocalDate DAYOFFFILMDATE = LocalDate.of(1895, 12, 28);
 
     @GetMapping
     public Collection<Film> findAll() {
@@ -38,7 +38,7 @@ public class FilmController {
             throw new ValidationException("Описание фильма слишком длинное");
         }
 
-        if (film.getReleaseDate().isBefore(DAYOFF)) {
+        if (film.getReleaseDate().isBefore(DAYOFFFILMDATE)) {
             log.error("Указана дата выпуска до 28.12.1895");
             throw new ValidationException("Дата выпуска фильма слишком ранняя");
         }
@@ -87,7 +87,7 @@ public class FilmController {
                 log.error("Название фильма больше 200 символов");
                 throw new ValidationException("Описание фильма слишком длинное");
             }
-            if (newFilm.getReleaseDate().isBefore(DAYOFF)) {
+            if (newFilm.getReleaseDate().isBefore(DAYOFFFILMDATE)) {
                 log.error("Указана дата выпуска до 28.12.1895");
                 throw new ValidationException("Дата выпуска фильма слишком ранняя");
             }
