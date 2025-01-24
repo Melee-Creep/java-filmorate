11490da858e092f0c729fb8ca8fbf291d5f6c563
xawk <xawk0469@yandex.ru>
2025-01-24 15:28:31 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mpa/MpaDbStorage.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class MpaDbStorage implements MpaStorage{
+public class MpaDbStorage implements MpaStorage {
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
